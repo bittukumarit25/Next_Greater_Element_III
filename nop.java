679c58582c5f5d44f8fbe498c930db16ea7c09f7
bittukumarit25 <121707439+bittukumarit25@users.noreply.github.com>
2023-10-05 22:14:48 +0530

    Add files via upload

---
+++ nop.java
@@ -0,0 +1,13 @@
+public class nop {
+    int number=4;
+    public static void main(String[] args) {
+        for(int i=1;i<=4;i++){
+            for(int number=1;number<=i;number++){
+                         System.out.print(number);
+
+            }
+                     System.out.println("");
+
+        }
+    }
+}
